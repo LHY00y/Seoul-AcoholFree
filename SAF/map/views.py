42e83449bd85cdcbc6852bfc3c9e4790fe18650a
LHY00y <leeha0038@gmail.com>
2023-06-09 16:49:38 +0900

    pull을 위한 commit

--- SAF/map/views.py
+++ SAF/map/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import render
 import pandas as pd
+from pyproj import Proj, transform
+
+proj_UTMK = Proj(init='epsg:2097')
+proj_WGS84 = Proj(init='epsg:4326')
 
 
 def index(request):
     # request.id 랑 같은 코드의 경찰서 정보가져오기
     # context에 정보 넘기기
-
-    # 좌표정보가 이상해서 api로 주소 > 위도경도 변환을 해야할듯
-    df = pd.read_csv(
+    store_df = pd.read_csv(
         'C:\\Users\\leeha\\workspace\\django\\Seoul-AcoholFree\\SAF\\static\\data\\서울시 단란주점영업 인허가 정보.csv', encoding='cp949')
-    df = df[df['상세영업상태코드'] == 1]
-    test = df[['사업장명', '좌표정보(X)', '좌표정보(Y)']
-              ][df['도로명주소'].str.contains('신림로', na=False)]
-    test.columns = ['사업장명', '위도', '경도']
-    dt = test.to_dict('records')
+    store_df = store_df[store_df['상세영업상태코드'] == 1]
+    store_df = store_df[['사업장명', '좌표정보(X)', '좌표정보(Y)']
+                        ][store_df['도로명주소'].str.contains('신림로', na=False)]
+    store_df.columns = ['사업장명', '위도', '경도']
+
+    # 중부원점 좌표계 > WGS84경위도 변환
+    store_df['경도'], store_df['위도'] = transform(
+        proj_UTMK, proj_WGS84, store_df['위도'], store_df['경도'])
+
+    # 딕셔너리 자료형으로 변환
+    store_dt = store_df.to_dict('records')
+
     context = {
-        'dt': dt
+        'store_dt': store_dt
     }
     return render(request, 'map/index.html', context)
