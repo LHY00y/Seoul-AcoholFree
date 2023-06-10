aa6672b7efaef2c9147410a3d851ad19bf5db659
LHY00y <leeha0038@gmail.com>
2023-06-10 16:02:08 +0900

    경찰서 초기데이터
    처음 실행하기 전 데이터 셋업. (시간이 다소 걸림)

--- SAF/map/views.py
+++ SAF/map/views.py
@@ -1,19 +1,23 @@
-from django.shortcuts import render
-import pandas as pd
+from django.shortcuts import render, HttpResponse
 from .seoulData.store import store
 from .seoulData.datas import RTdata
 
 
 def index(request):
+    if not request.user.is_active:
+        msg = '<script>'
+        msg += 'alert("로그인 후 이용해주세요");'
+        msg += 'location.href="/..";'
+        msg += '</script>'
+        return HttpResponse(msg)
     # request.id 랑 같은 코드의 경찰서 정보가져오기
     # context에 정보 넘기기
-    
 
     store_dt = store("신림로",
                      "..\\SAF\\static\\data\\서울시 단란주점영업 인허가 정보.csv") + store("신림로", '..\\SAF\\static\\data\\서울시 유흥주점영업 인허가 정보.csv')
 
     RTdat = RTdata()
-    
+
     # 로그인한 지부에 맞는 police객체 전달
 
     context = {
