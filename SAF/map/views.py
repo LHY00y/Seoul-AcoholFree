1317f7eae0cbb75d333cdc1d3c0b92139003de62
LHY00y <leeha0038@gmail.com>
2023-06-13 17:47:53 +0900

    로그인 사용자에 따라 초기위치변경
    로그인한 경찰서 소재에 맞춰 지도 초기 center값 변경

--- SAF/map/views.py
+++ SAF/map/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from .seoulData.store import store
 from .seoulData.datas import RTdata
+from map.models import Police
 
 
 def index(request):
@@ -10,17 +11,21 @@ def index(request):
         msg += 'location.href="/..";'
         msg += '</script>'
         return HttpResponse(msg)
-    # request.id 랑 같은 코드의 경찰서 정보가져오기
-    # context에 정보 넘기기
 
-    store_dt = store("신림로",
-                     "..\\SAF\\static\\data\\서울시 단란주점영업 인허가 정보.csv") + store("신림로", '..\\SAF\\static\\data\\서울시 유흥주점영업 인허가 정보.csv')
+    if request.user.username == 'admin':
+        store_dt = None
+        police_table = {'address': '서울특별시 서대문구 통일로 97 경찰청'}
+    else:
+        police_table = Police.objects.get(code=request.user.username)
+        store_dt = store(police_table.address.split()[1],
+                         "..\\SAF\\static\\data\\서울시 단란주점영업 인허가 정보.csv") + store(police_table.address.split()[1], '..\\SAF\\static\\data\\서울시 유흥주점영업 인허가 정보.csv')
 
     RTdat = RTdata()
 
     # 로그인한 지부에 맞는 police객체 전달
 
     context = {
+        'police_table': police_table,
         'store_dt': store_dt,
         'pops': RTdat[0].to_dict('records'),
         'roads': RTdat[1].to_dict('records'),
